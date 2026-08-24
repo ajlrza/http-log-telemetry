ad113b7afa96f85c3dec26773030e5c588c17db9
aj <158788527+ajlrza@users.noreply.github.com>
2026-08-24 18:23:48 +0800

    Implement logs_parser function in parser.rs

---
+++ parser.rs
@@ -0,0 +1,26 @@
+fn logs_parser(log: &str) {
+    
+    let keywords_map: [str; 6] = [""; 6];
+    
+    keywords_map[0] = "GET";
+    keywords_map[1] = "POST";
+    keywords_map[2] = "DELETE";
+    
+    keywords_map[3] = "Host";
+    keywords_map[4] = "Authorization";
+    keywords_map[5] = "---";
+    
+    let current_str_state: &str = "";
+    let whitespace: bool  false;
+    
+    for word in log {
+        if (word == keywords_map.next()) {
+            current_str_state = current_str_state + word;
+            continue;
+        }
+        else if (word == "") {
+            
+        }
+    }
+    
+}
